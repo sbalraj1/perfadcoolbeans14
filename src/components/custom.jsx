9245e819766c7116ef40564a05b773fa9cd3cd27
Neha Priyadarshini <npriyadarshini@twitter.com>
2022-04-25 19:42:43 -0700

    adding PEP new tag info and SET tag info

---
+++ src/components/custom.jsx
@@ -0,0 +1,7 @@
+export default function Purchase() {
+    return (
+      <main style={{ padding: '1rem 0' }}>
+        <h2>Lets purchase this and all other stuff!!</h2>
+      </main>
+    );
+  }
---
+++ src/components/home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <main style={{ padding: '1rem 0' }}>
+        <h2>Lets get going!</h2>
+      </main>
+    );
+  }
